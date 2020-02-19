df02afa8463b625c5fea45bf0d5c173e2219ecde
Idunna <idunna@cryptogroup.net>
2020-02-19 22:25:38 +0000

    add vhcchain
    And update vendor directory / add dependencies.

--- go.mod
+++ go.mod
@@ -8,22 +8,20 @@ require (
 	github.com/btcsuite/goleveldb v1.0.0
 	github.com/btcsuite/winsvc v1.0.0
 	github.com/davecgh/go-spew v1.1.1
-	github.com/dchest/blake256 v1.0.0
+	github.com/dchest/blake256 v1.1.0
 	github.com/dchest/siphash v1.2.1
-	github.com/decred/base58 v1.0.0
+	github.com/decred/base58 v1.0.2
 	github.com/decred/slog v1.0.0
+	github.com/frankbraun/codechain v1.0.1
 	github.com/golang/protobuf v1.3.2 // indirect
-	github.com/gorilla/websocket v1.4.0
+	github.com/gorilla/websocket v1.4.1
 	github.com/jessevdk/go-flags v1.4.0
 	github.com/jrick/bitset v1.0.0
 	github.com/jrick/logrotate v1.0.0
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/onsi/ginkgo v1.8.0 // indirect
 	github.com/onsi/gomega v1.5.0 // indirect
-	golang.org/x/crypto v0.0.0-20190701094942-4def268fd1a4
-	golang.org/x/net v0.0.0-20190628185345-da137c7871d7 // indirect
-	golang.org/x/sys v0.0.0-20190712062909-fae7ac547cb7 // indirect
+	golang.org/x/crypto v0.0.0-20200214034016-1d94cc7ab1c6
 	golang.org/x/text v0.3.2 // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
-	gopkg.in/yaml.v2 v2.2.2 // indirect
 )
--- vendor/github.com/decred/base58/base58.go
+++ vendor/github.com/decred/base58/base58.go
@@ -1,76 +1,118 @@
 // Copyright (c) 2013-2015 The btcsuite developers
-// Copyright (c) 2015 The Decred developers
+// Copyright (c) 2015-2019 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
 
 package base58
 
-import (
-	"math/big"
-)
-
 //go:generate go run genalphabet.go
 
-var bigRadix = big.NewInt(58)
-var bigZero = big.NewInt(0)
-
 // Decode decodes a modified base58 string to a byte slice.
-func Decode(b string) []byte {
-	answer := big.NewInt(0)
-	j := big.NewInt(1)
+func Decode(input string) []byte {
+	if len(input) == 0 {
+		return []byte("")
+	}
 
-	scratch := new(big.Int)
-	for i := len(b) - 1; i >= 0; i-- {
-		tmp := b58[b[i]]
-		if tmp == 255 {
+	// The max possible output size is when a base58 encoding consists of
+	// nothing but the alphabet character at index 0 which would result in the
+	// same number of bytes as the number of input chars.
+	output := make([]byte, len(input))
+
+	// Encode to base256 in reverse order to avoid extra calculations to
+	// determine the final output size in favor of just keeping track while
+	// iterating.
+	var index int
+	for _, r := range input {
+		// Invalid base58 character.
+		val := uint32(b58[r])
+		if val == 255 {
 			return []byte("")
 		}
-		scratch.SetInt64(int64(tmp))
-		scratch.Mul(j, scratch)
-		answer.Add(answer, scratch)
-		j.Mul(j, bigRadix)
-	}
 
-	tmpval := answer.Bytes()
-
-	var numZeros int
-	for numZeros = 0; numZeros < len(b); numZeros++ {
-		if b[numZeros] != alphabetIdx0 {
-			break
+		// Multiply each byte in the output by 58 and encode to base256 while
+		// propagating the carry.
+		for i, b := range output[:index] {
+			val += uint32(b) * 58
+			output[i] = byte(val)
+			val >>= 8
+		}
+		for ; val > 0; val >>= 8 {
+			output[index] = byte(val)
+			index++
 		}
 	}
-	flen := numZeros + len(tmpval)
-	val := make([]byte, flen)
-	copy(val[numZeros:], tmpval)
 
-	return val
+	// Account for the leading zeros in the input.  They are appended since the
+	// encoding is happening in reverse order.
+	for _, r := range input {
+		if r != alphabetIdx0 {
+			break
+		}
+
+		output[index] = 0
+		index++
+	}
+
+	// Truncate the output buffer to the actual number of decoded bytes and
+	// reverse it since it was calculated in reverse order.
+	output = output[:index:index]
+	for i := 0; i < index/2; i++ {
+		output[i], output[index-1-i] = output[index-1-i], output[i]
+	}
+
+	return output
 }
 
 // Encode encodes a byte slice to a modified base58 string.
-func Encode(b []byte) string {
-	x := new(big.Int)
-	x.SetBytes(b)
+func Encode(input []byte) string {
+	// Since the conversion is from base256 to base58, the max possible number
+	// of bytes of output per input byte is log_58(256) ~= 1.37.  Thus, the max
+	// total output size is ceil(len(input) * 137/100).  Rather than worrying
+	// about the ceiling, just add one even if it isn't needed since the final
+	// output is truncated to the right size at the end.
+	output := make([]byte, (len(input)*137/100)+1)
 
-	answer := make([]byte, 0, len(b)*136/100)
-	for x.Cmp(bigZero) > 0 {
-		mod := new(big.Int)
-		x.DivMod(x, bigRadix, mod)
-		answer = append(answer, alphabet[mod.Int64()])
+	// Encode to base58 in reverse order to avoid extra calculations to
+	// determine the final output size in favor of just keeping track while
+	// iterating.
+	var index int
+	for _, r := range input {
+		// Multiply each byte in the output by 256 and encode to base58 while
+		// propagating the carry.
+		val := uint32(r)
+		for i, b := range output[:index] {
+			val += uint32(b) << 8
+			output[i] = byte(val % 58)
+			val /= 58
+		}
+		for ; val > 0; val /= 58 {
+			output[index] = byte(val % 58)
+			index++
+		}
 	}
 
-	// leading zero bytes
-	for _, i := range b {
-		if i != 0 {
+	// Replace the calculated remainders with their corresponding base58 digit.
+	for i, b := range output[:index] {
+		output[i] = alphabet[b]
+	}
+
+	// Account for the leading zeros in the input.  They are appended since the
+	// encoding is happening in reverse order.
+	for _, r := range input {
+		if r != 0 {
 			break
 		}
-		answer = append(answer, alphabetIdx0)
+
+		output[index] = alphabetIdx0
+		index++
 	}
 
-	// reverse
-	alen := len(answer)
-	for i := 0; i < alen/2; i++ {
-		answer[i], answer[alen-1-i] = answer[alen-1-i], answer[i]
+	// Truncate the output buffer to the actual number of encoded bytes and
+	// reverse it since it was calculated in reverse order.
+	output = output[:index:index]
+	for i := 0; i < index/2; i++ {
+		output[i], output[index-1-i] = output[index-1-i], output[i]
 	}
 
-	return string(answer)
+	return string(output)
 }
